78075ffe600882b8101e4bf1a7c1797dda27d7a8
nirsa <nirsa11@gmail.com>
2020-04-12 21:37:17 +0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,10 +32,10 @@
                             <div class="card-body">
                                 {{-- <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1> --}}
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>Bulid your best profil   <img src="images/vstu.png" width="15px" height="20px"></li>
-                                    <li>Find the dream job  <img src="images/vstu.png" width="15px" height="20px"></li>
-                                    <li>Get important information  <img src="images/vstu.png" width="15px" height="20px"></li>
-                                    <li>Feedback for  course content  <img src="images/vstu.png" width="15px" height="20px"></li>
+                                    <li>Bulid your best profil   <img src="{{asset('images/vstu.png')}}" width="15px" height="20px"></li>
+                                    <li>Find the dream job  <img src="{{asset('images/vstu.png')}}" width="15px" height="20px"></li>
+                                    <li>Get important information  <img src="{{asset('images/vstu.png')}}" width="15px" height="20px"></li>
+                                    <li>Feedback for  course content  <img src="{{asset('images/vstu.png')}}" width="15px" height="20px"></li>
                                 </ul>
                                 <a type="button" href="{{url('register?role=student')}}" class="btn btn-lg btn-block btn-outline-success ">Sign up for Student</a>
                             </div>
@@ -47,10 +47,10 @@
                             <div class="card-body">
                                 {{-- <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1> --}}
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>We did half the work for you  <img src="images/vdep.png" width="15px" height="20px"></li>
-                                    <li>Update information  <img src="images/vdep.png" width="15px" height="20px"></li>
-                                    <li>Continue after adjustment  <img src="images/vdep.png" width="15px" height="20px"></li>
-                                    <li>Control of profiles and job  <img src="images/vdep.png" width="15px" height="20px"></li>
+                                    <li>We did half the work for you  <img src="{{asset('images/vdep.png')}}" width="15px" height="20px"></li>
+                                    <li>Update information  <img src="{{asset('images/vdep.png')}}" width="15px" height="20px"></li>
+                                    <li>Continue after adjustment  <img src="{{asset('images/vdep.png')}}" width="15px" height="20px"></li>
+                                    <li>Control of profiles and job  <img src="{{asset('images/vdep.png')}}" width="15px" height="20px"></li>
                                 </ul>
                                 <a type="button" href="{{url('register?role=department')}}" class="btn btn-lg btn-block btn-outline-warning">Sign up for Department</a>
                             </div>
@@ -62,10 +62,10 @@
                             <div class="card-body">
 
                                 <ul class="list-unstyled mt-3 mb-4">
-                                    <li>Create a new job <img src="images/vemp.png" width="15px" height="20px"></li>
-                                    <li>Find Student by category <img src="images/vemp.png" width="15px" height="20px"></li>
-                                    <li>Edit and Delete Your job <img src="images/vemp.png" width="15px" height="20px"></li>
-                                    <li>Add like to profil student <img src="images/vemp.png" width="15px" height="20px"></li>
+                                    <li>Create a new job <img src="{{asset('images/vemp.png')}}" width="15px" height="20px"></li>
+                                    <li>Find Student by category <img src="{{asset('images/vemp.png')}}" width="15px" height="20px"></li>
+                                    <li>Edit and Delete Your job <img src="{{asset('images/vemp.png')}}" width="15px" height="20px"></li>
+                                    <li>Add like to profil student<img src="{{asset('images/vemp.png')}}" width="15px" height="20px"></li>
                                 </ul>
                                 <a href="{{url('register?role=employer')}}" type="button" class="btn btn-lg btn-block btn-outline-danger">Sign up for employer</a>
                             </div>
