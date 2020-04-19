0b936a36621b16a0552bf5432516acf8777c1891
nirsa <nirsa11@gmail.com>
2020-04-19 22:50:09 +0300

    wip

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,41 @@
 window.onload = function init(){
- 
-    
-        // function present(PRESENT) {
-        //     let count = []
-        //     for (const [key, value] of Object.entries(PRESENT)) {
-        //         count.push(key)
-        //         console.log(count);
-        //         let div = document.querySelector(`.${key}`);
-        //         let color = $(div).data("color");
-        //         $(div).css("background", color)
-        //         div.style.transition = 'background 0.5s ease-in-out'
-        //         // if(div.style.background != '#fff'){
-        //         //     div.style.order = 1
-        //         // };
-        //     }
-        //     let presentNum = Math.ceil(count.length * 14 + 2)
-        //     let presentDiv = document.querySelector('.present');
-        //     presentDiv.innerHTML = presentNum + '% ';
-        //     let r = 156 - (count.length * 4);
-        //     let g = 233 - (count.length * 2);
-        //     let b = 125 - (count.length * 6);
-        //     let colorPresentDiv = "rgba(" + r + "," + g + "," + b + "," + (count.length + 2) / 10 + ")";
-        //     console.log(colorPresentDiv)
-        //     $(presentDiv).css("background", colorPresentDiv)
-        //
-        //     if (presentNum == 100) {
-        //         swal({
-        //             position: 'top-center',
-        //             icon: 'success',
-        //             title: 'Your profile has been completed... Now find Job!',
-        //             showConfirmButton: false,
-        //             timer: 3000
-        //         })
-        //     }
-        // }
-        //
-        // present(PRESENT)
+
+
+        function present(PRESENT) {
+            let count = []
+            for (const [key, value] of Object.entries(PRESENT)) {
+                count.push(key)
+                console.log(count);
+                let div = document.querySelector(`.${key}`);
+                let color = $(div).data("color");
+                $(div).css("background", color)
+                div.style.transition = 'background 0.5s ease-in-out'
+                // if(div.style.background != '#fff'){
+                //     div.style.order = 1
+                // };
+            }
+            let presentNum = Math.ceil(count.length * 14 + 2)
+            let presentDiv = document.querySelector('.present');
+            presentDiv.innerHTML = presentNum + '% ';
+            let r = 156 - (count.length * 4);
+            let g = 233 - (count.length * 2);
+            let b = 125 - (count.length * 6);
+            let colorPresentDiv = "rgba(" + r + "," + g + "," + b + "," + (count.length + 2) / 10 + ")";
+            console.log(colorPresentDiv)
+            $(presentDiv).css("background", colorPresentDiv)
+
+            if (presentNum == 100) {
+                swal({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Your profile has been completed... Now find Job!',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+            }
+        }
+
+        present(PRESENT)
 
     }
 
